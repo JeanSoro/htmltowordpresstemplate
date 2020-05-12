621f41d4a0814e889901497c1fa96ecf1901fd4b
Jean <jean.soro007@gmail.com>
2020-05-12 16:01:11 -0400

    Added theme support for post thumbnails

--- header.php
+++ header.php
@@ -22,10 +22,11 @@
 <body>
     
     <header class="header text-center">	    
-		<a class="site-title pt-lg-4 mb-0" href="index.html">SiteName.dev</a>
+		<a class="site-title pt-lg-4 mb-0" href="index.html">
+			<?php echo get_bloginfo('name'); ?>
+		</a>
 		
 
-        
 	    <nav class="navbar navbar-expand-lg navbar-dark" >
            
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
